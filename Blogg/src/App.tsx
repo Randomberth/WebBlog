acb22b737e12c49051fd4b82e8745ea53bcf031f
Alibert <aliberth33@gmail.com>
2024-09-04 06:06:58 -0400

    Test Cambio de directorio

--- Blogg/src/App.tsx
+++ Blogg/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="font-bold text-violet-500">
-        Tailwind instalado
+        Test Cambio de directorio
       </p>
     </>
   )
